f85ec495cc4d01817e6f26f8cfaceb40b12a89d1
Javierdiaz6g <javierdiaz6@gmail.com>
2026-07-10 16:16:55 -0500

    Envio taller parte de extra por Geova

--- main.py
+++ main.py
@@ -1,6 +1,55 @@
+print("Escriba S para sumar, R para restar, M para multiplicar, D para dividir y P para obtener el residuo pero de dos numeros")
+
+if(input() == "S"):
+
+    print("Escriba un numero")
+    num1 = int(input())
+    print("Escriba otro numero")
+    num2 = int(input())
+    resultado = num1 + num2
+    print(resultado)
+elif(input() == "R"):
+
+
+    print("Escriba un numero")
+    num1 = int(input())
+    print("Escriba otro numero")
+    num2 = int(input())
+    resultado = num1 - num2
+    print(resultado)
+elif(input() == "M"):
+
+    print("Escriba un numero")
+    num1 = int(input())
+    print("Escriba otro numero")
+    num2 = int(input())
+    resultado = num1 * num2
+    print(resultado)
+elif(input() == "D"):
+
+    print("Escriba un numero")
+    num1 = int(input())
+    print("Escriba otro numero")
+    num2 = int(input())
+    resultado = num1 / num2
+    print(resultado)
+elif(input() == "P"):
+
+    print("Escriba un numero")
+    num1 = int(input())
+    print("Escriba otro numero")
+    num2 = int(input())
+    resultado = num1 % num2
+    print(resultado)
+
+print("Ahora realicemos suma 3 numeros")
+
 print("Escriba un numero")
 num1 = int(input())
-print("Escriba otro numero")
+print("Escriba segundo numero")
 num2 = int(input())
-resultado = num1 + num2
-print(resultado)
+print("Escriba el tercer numero")
+num3 = int(input())
+resultado2 = num1 + num2 + num3
+print(resultado2)
+
